af50b1b273b55ee7d0c2962664a924b083d32132
Feehzin <felipe-familia123@hotmail.com>
2025-01-28 11:09:39 -0300

    atualizando

--- main/Node.h
+++ main/Node.h
@@ -8,6 +8,12 @@ struct Node
   int linha;
   int coluna;
   double valor;
+
+  Node(int linha, int coluna, double valor)
+    : direita{this}, abaixo{this}, linha{linha}, coluna{coluna}, valor{valor}
+  {}
 };
 
 #endif
+
+//direita e abaixo recebem ele mesmos, apontando para o próprio nó (indicando que, inicialmente, forma-se uma uma lista circular para ele mesmo)
