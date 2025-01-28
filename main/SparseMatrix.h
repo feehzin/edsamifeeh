c9a7f484428eae46cc0c10addbbbeeaeec937b0e
Feehzin <felipe-familia123@hotmail.com>
2025-01-28 11:27:55 -0300

    atualizando

--- main/SparseMatrix.h
+++ main/SparseMatrix.h
@@ -1,10 +1,26 @@
 #ifndef SPARSEMATRIX_H
 #define SPARSEMATRIX_H
+#include <iostream>
 #include "Node.h"
 
 class SparseMatrix
 {
+  private:
+    Node* m_head; //nó sentinela
+    int linhas; //número de linhas
+    int colunas; //números de colunas
+  public:
+    //Construtor
+    SparseMatrix(int linhas, int colunas)
+    {
+      if(linhas <= 0 || colunas <= 0)
+      {
+        throw std::out_of_range("Numero de linhas e/ou colunas invalidas");
+      }
 
+      this->linhas = linhas;
+      this->colunas = colunas;
+    }
 };
 
 #endif
