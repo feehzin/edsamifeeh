b07fe5053d7b747342f9b4669a92bc06ec801c58
Feehzin <felipe-familia123@hotmail.com>
2025-02-02 00:49:29 -0300

    atualização

--- main/main.cpp
+++ main/main.cpp
@@ -4,15 +4,29 @@ using namespace std;
 
 int main() {
   
-    void readSparseMatrix(SparseMatrix& m,std::string nomedoarquivo);
-
-    SparseMatrix sum(SparseMatrix&A, SparseMatrix&B)
+    void readSparseMatrix(SparseMatrix& m,std::string nomedoarquivo)
     {
-
 
     }
 
-    SparseMatrix multiply(SparseMatrix&A, SparseMatrix&B);
+    SparseMatrix sum(SparseMatrix&A, SparseMatrix&B)
+    {
+        SparseMatrix C;
+
+        for (int i = 0; i < A.m_head->linhas; i++)
+        {
+            for (int j = 0; j < A.m_head->colunas; j++)
+            {
+                double soma += A.get(i, j) + B.get(i, j)
+            }
+        }
+
+    }
+
+    SparseMatrix multiply(SparseMatrix&A, SparseMatrix&B)
+    {
+
+    }
 
     return 0;
 }
