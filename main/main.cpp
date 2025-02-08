a1fc58de52f4deb7b354f4fe2567c7ecb502bec2
SamirinhaG <samiragomes@alu.ufc.br>
2025-02-08 20:52:39 -0300

    Update main.cpp
    atualização

--- main/main.cpp
+++ main/main.cpp
@@ -114,21 +114,38 @@ void readSparseMatrix(SparseMatrix& m, std::string nome_do_arquivo){
   }
 
      
- SparseMatrix multiply(SparseMatrix& A, SNodeparseMatrix& B){
-    if (A.getColunas()!= B.getLinhas()){
-      throw std::out_of_range("O número de colunas de A deve ser igual ao número de  linhas de B para executar a multiplicação.");
+  SparseMatrix multiply(SparseMatrix& A, SparseMatrix& B) {
+    if (A.getColunas() != B.getLinhas()) {
+        throw std::out_of_range("O número de colunas de A deve ser igual ao número de linhas de B para executar a multiplicação.");
     }
 
-    SparseMatrix C(A.getLinhas(), B.getColunas());  
+    SparseMatrix C(A.getLinhas(), B.getColunas());
 
-    // loop para percorrer todas a linhas de A
-    for(Node* element_A = A.getHead()->abaixo; linha_A != A.getHead(); linha_A = linha_A->abaixo){
-      int i = element_A->linhas;
-      int j = element_A->colunas;
-      double valorA = element_A->valor;
+    for (Node* linha_A = A.getHead()->abaixo; linha_A != A.getHead(); linha_A = linha_A->abaixo) {
+        for (Node* element_A = linha_A->direita; element_A != linha_A; element_A = element_A->direita) {
+            int i = element_A->linha;
+            int j = element_A->coluna;
+            double valorA = element_A->valor;
+
+            Node* coluna_B = B.getHead()->abaixo;
+            while (coluna_B != B.getHead() && coluna_B->linha != j) {
+                coluna_B = coluna_B->abaixo;
+            }
+
+            if (coluna_B->linha == j) {
+                for (Node* element_B = coluna_B->direita; element_B != coluna_B; element_B = element_B->direita) {
+                    int s = element_B->coluna;
+                    double valorB = element_B->valor;
+
+                    double valorC = C.get(i, s) + (valorA * valorB);
+                    C.insert(i, s, valorC);
+                }
+            }
+        }
     }
-  }
 
+    return C;
+}
 int main()
 {
   vector<SparseMatrix> matriz;
