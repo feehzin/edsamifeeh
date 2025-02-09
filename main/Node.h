f2b3879b68632be05aacbf6fff5c42daadcc54dc
Feehzin <felipe-familia123@hotmail.com>
2025-02-09 16:02:54 -0300

    Co-authored-by: SamirinhaG <SamirinhaG@users.noreply.github.com>

--- main/Node.h
+++ main/Node.h
@@ -4,17 +4,17 @@
 /*Estrutura que representa um nó da mariz esparsa.
 Cada nó armazena um valor não nulo e mantém ponteiros para
 os próximos elementos na mesma linha e na mesma coluna.*/ 
-{
+struct Node{
   Node *direita; // Ponteiro para o próximo elemento da mesma linha.
   Node *abaixo; // Ponteiro  para o próximo elemento da mesma coluna.
   int linha; // Índice da linha do elemento.
-  int coluns; // Índice da coluna do elemento.
+  int coluna; // Índice da coluna do elemento.
   double valor; // Valor armazenado no nó (valor não nulo).
 
   /*Construtor que inicializa um nó com valores específicos.
   Os ponteiros direita e abaixo recebem ele mesmos(this), apontando para o próprio nó 
   (indicando que, inicialmente, forma-se uma lista circular para ele mesmo)*/
-  Node(int linhas, int colunas, double valor)
+  Node(int linha, int coluna, double valor)
     : direita{this}, abaixo{this}, linha{linha}, coluna{coluna}, valor{valor} {}
 };
 
