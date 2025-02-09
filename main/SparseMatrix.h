677ec66e7eace5936f5f9387ae14454b62a1909f
SamirinhaG <samiragomes@alu.ufc.br>
2025-02-09 11:54:01 -0300

    Update SparseMatrix.h
    atualização

--- main/SparseMatrix.h
+++ main/SparseMatrix.h
@@ -1,15 +1,15 @@
 #ifndef SPARSEMATRIX_H
 #define SPARSEMATRIX_H
-#include <iostream>
 #include "Node.h"
+#include <iostream>
 #include <stdexcept> // Biblioteca para lançar exceções
 
 /**Funções obrigatorias da classe SparseMatrix:
- * Construtor
- * Destrutor
- * Inserção de elementos
- * Obter valor
- * Impressão
+ * Construtor: Inicializa a matriz esparsa com um nó sentinela.
+ * Destrutor: Libera a memória alocada corretamente.
+ * Inserção de elementos: Insere valores diferentes de zero na estrutura.
+ * Obter valor: Retorna um elemento específico da matriz.
+ * Impressão: Exibe a matriz esparsa de forma legível.
  */
 
 // Classe que respresenta a matriz esparsa
@@ -19,176 +19,104 @@ class SparseMatrix {
     int linhas, colunas; // Dimensões da matriz esparsa
 
   public:
-    // get que copia o m_head para usar na função
-    Node* getHead() const{
-      return m_head;
-    }
-    //Construtor (cria uma matriz vazia)
+    
+    /*Construtor (cria uma matriz vazia).
+    Para m Número de linhas.
+    Para n Número de colunas.
+    throws std::out_of_range se os valores forem inválidos.*/
     SparseMatrix(int m, int n){
       if(m <= 0 || n <= 0)
       {
         throw std::out_of_range("Numero de linhas e/ou colunas invalidas.");
       }
-
+      linhas = m;
+      colunas = n;
       m_head = new Node(0, 0, 0);
-      m_head->linhas = m;
-      m_head->colunas = n;
-      
-      if (m_head == nullptr)
-      {
-        throw std::out_of_range("Erro ao alocar memoria.");
+      if (!m_head){
+        throw std::bad_alloc(); // Garante que a alocação foi bem-sucedida
+    }
+      m_head->direita = m_head; // Lista circular na horizontal
+      m_head->abaixo = m_head; // Lista circular na vertical
+    }
+  
+    //Destrutor: libera a memória alocada dinamicamente.
+    ~SparseMatrix() {
+      if (!m_head) return;
+      Node* atual_L = m_head->abaixo;
+      while (atual_L != m_head) {
+          Node* atual_C = atual_L->direita;
+          while (atual_C != atual_L) {
+              Node* aux = atual_C;
+              atual_C = atual_C->direita;
+              delete aux; // Libera memória da célula
+          }
+          Node* aux = atual_L;
+          atual_L = atual_L->abaixo;
+          delete aux; // Libera memória da linha
       }
-      
-      m_head->direita = m_head; //aponta para si mesmo
-      m_head->abaixo = m_head; //aponta para si mesmo
-    }
-
-    //Função que retorna o número de linhas de uma matriz esparsa
-    int getLinhas(){
-      return linhas;
-    }
-
-    //Função que retorna o número de colunas de uma matriz esparsa
-    int getColunas(){
-      return colunas;
-    }
-
-
-    //Destrutor: libera a memória alocada
-    ~SparseMatrix(){
-      if(!m_head) {
-        throw std::out_of_range("Lista vazia.");
-      } 
-
-      Node* atual_c = m_head->abaixo; //recebe a primeira linha
-
-      //percorre as linhas da matriz
-      while(atual_c != m_head){
-    
-        Node* atual_l = atual_c->direita;
-
-        //percorre até retornar para o início da linha
-        while (atual_l != atual_c)
-        {
-          Node* aux_l = atual_l->direita; //guarda o prócimo nó antes de deletá-lo
-          delete atual_l;
-          atual_l = aux_l; //atual_l avança para o próximo nó
-        }
-
-        Node* aux_c = atual_c->abaixo; //guarda a próxima linha antes de deletar o atual
-        delete atual_c; // deleta o nó da coluna
-        atual_c = aux_c; //avança para a próxima coluna
-      }
-
-      delete m_head;
+      delete m_head; // Libera o nó sentinela
       m_head = nullptr;
-    }
-       
-    //Função que insere um valor na matriz esparsa
-    void insert(int i, int j, double value)
-    {
-      if(value == 0){
-       // Não aceita inserir 0 na matriz
-      throw std::out_of_range("Valor invalido.");
-      }
-      
-      if(i < 1 || i > linhas || j < 1 || j > colunas){ 
-        // Impede que os indices acessados sejam negativos ou indices que a matriz nao suporta 
-      throw std::out_of_range("Indices invalidos.");
-    }
-    // Ponteiros auxiliares que percorre a lista de linhas
-    Node* atual_c = m_head->abaixo; // Recebe a primeira linha da matriz
-    Node* auxguard = m_head; //auxguard guarda a linha anterior para possiveis inserções
+  }
+    // Retorna o número de linhas de uma matriz.
+    int getLinhas() const { return linhas; }
 
-    // Percorre as linhas ate encontrar a linha desejada ou passar por ela
-    while(atual_c != m_head && atual_c->linhas < i){
-      auxguard = atual_c; // Atualiza o ponteiro auxiliar para manter a referenca da linha anterior
-      atual_c = atual_c->abaixo; // Move para proxima linha abaixo
-    }
+    // Retorna o número de colunas de uma matriz.
+    int getColunas() const { return colunas; }
+  
+     // Retorna o ponteiro para o nó sentinela da matriz.
+     Node* getHead() const { return m_head; }
 
-    // Verifica se a linha já existe ou precisa ser criada
-    if(atual_c == m_head || atual_c->linhas != i){ // Cria uma nova linha, pois a linha i ainda não existe
-      Node* novaLinha = new Node(i, 0, 0); // 0 na coluna indica o nó de controle da linha
-      novaLinha->abaixo = atual_c; // O novo nó aponta para próxima linha
-      auxguard->abaixo = novaLinha; // A linha anterioe aponta para a nova linha
-      atual_c = novaLinha; // Atualiza a atual_c para apontar para a nova linha criada
+    // Função que insere um valor na matriz esparsa
+    void insert(int i, int j, double value){
+    if (value == 0) return;
+    if (i < 1 || i > linhas || j < 1 || j > colunas) {
+        throw std::out_of_range("Índices fora dos limites da matriz.");
     }
-    // Ponteiros auxiliares para percorrer as colunas dentro da linha
-    Node* atual = atual_c->direita; // Começa no primeiro elemento da linha
-    Node* anterior = atual_c; // Guarda o nó anterior na linha
-
-    // Percorre a linha ate achhar a coluna desejada ou passar por ela
-    while(atual != atual_c && atual->colunas < j){ // O loop avança pelas colunas ate achar a posição de j ou passar por ela  
-      anterior = atual; // Atualiza o ponteiro anterior para manter a referencia do nó anterior na linha
-      atual = atual->direita; //Move para a proxima coluna 
+    Node* atual_L = m_head;
+    while (atual_L->abaixo != m_head && atual_L->abaixo->linha < i) {
+        atual_L = atual_L->abaixo;
     }
-
-    // Verifica se o elemento já existe na posição i e j
-    if(atual != atual_c && atual->colunas  == j){
-      atual->valor = value; // Se já existe, atualiza o valor
-      return;
+    if (atual_L->abaixo == m_head || atual_L->abaixo->linha != i) {
+        Node* novaLinha = new Node(i, -1, 0);
+        novaLinha->abaixo = atual_L->abaixo;
+        atual_L->abaixo = novaLinha;
     }
-
-    // Se o elemeento não existe, se cria um novo nó para guardar o valor
-    Node* novoNode = new Node(i, j, value); // Cria um nó novo com os valores fornecidos
-    novoNode->direita = atual; // O novo nó aponta para o proximo elemento na linha
-    anterior->direita = novoNode; // O nó anterior aponta para o novo nó , inoserindo ele corretamente
-    }
+    atual_L = atual_L->abaixo;
     
-    //Retorna um valor da matriz
-    double get(int i, int j)
-    {
-      //verifica se os índices são válidos
-      //senão, lança uma exceção
-      if (i < 1 || i > linhas || j < 1 || j > colunas)
-      {
-        throw std::out_of_range("Indice(s) invalido(s).");
-      }
-      
-      Node* aux = m_head->abaixo;
-      
-      //Percorre os nós (parte da coluna) até encontrar a linha i
-      //aux->linhas < i, significa que não chegamos, então faz o aux continuar percorrendo
-      while (aux != m_head && aux->linhas < i)
-      {
-        aux = aux->abaixo;
-      }
-      
-      //se aux == m_head, significa que foi-se até o fim da matriz sem achar a linha
-      //se aux->linhas != i,significa que a linha i não tem nenhum elemnto, diferente de zero, armazenado
-      if (aux == m_head || aux->linhas != i)
-      {
-        return 0.0;
-      }
-      
-      Node* elemento = aux->direita; //aux->direita nos leva ao primeiro elemto não nulo da linha
-
-      //Percorre os nós à direita ate encontrar a coluna j
-      //se elemento->colunas < j, significa que não chegou na coluna j, então faz o elemento continuar percorrendo
-      while (elemento != aux && elemento->colunas < j)
-      {
-        elemento = elemento->direita;
-      }
-      
-      //elemnto != aux nos garante que não retorne um elemento->valor não real
-      //aux aponta como nó sentinela para a linha
-      if (elemento != aux && elemento->colunas == j && elemento->linhas == i)
-      {
-        return elemento->valor;
-      }
-      else
-      {
-        return 0.0;
-      }
+    Node* atual_C = atual_L;
+    while (atual_C->direita != atual_L && atual_C->direita->coluna < j) {
+        atual_C = atual_C->direita;
     }
-
-    //Exibe a matriz no terminal
-    void print()
-    {
-      for (int i = 1; i <= linhas; i++)
-      {
-        for (int j = 1; j <= colunas; j++)
-        {
+    if (atual_C->direita == atual_L || atual_C->direita->coluna != j) {
+        Node* novoNode = new Node(i, j, value);
+        novoNode->direita = atual_C->direita;
+        atual_C->direita = novoNode;
+    } else {
+        atual_C->direita->valor = value;
+    }
+}
+    // Retorna um valor específico da matriz.
+    double get (int i, int j) const {
+      if (i < 1 || i > linhas || j < 1 || j > colunas) {
+        throw std::out_of_range("Índices fora dos limites da matriz.");
+    }
+    Node* atual_L = m_head->abaixo;
+    while (atual_L != m_head && atual_L->linha < i) {
+        atual_L = atual_L->abaixo;
+    }
+    if (atual_L == m_head || atual_L->linha != i) return 0.0;
+    
+    Node* atual_C = atual_L->direita;
+    while (atual_C != atual_L && atual_C->coluna < j) {
+        atual_C = atual_C->direita;
+    }
+    if (atual_C == atual_L || atual_C->coluna != j) return 0.0;
+    return atual_C->valor;
+}
+    // Exibe a matriz no terminal
+    void print(){
+      for (int i = 1; i <= linhas; i++){
+        for (int j = 1; j <= colunas; j++){
           double valor = get(i, j);
           std::cout << valor << " ";
         }
